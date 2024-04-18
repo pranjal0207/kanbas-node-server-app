cb0e1015af5102cdd1b5a2ee781f58316eab2326
Pranjal Rane <rane.pr@northeastern.edu>
2024-04-18 01:13:05 -0400

    add users and mongo

--- App.js
+++ App.js
@@ -1,3 +1,60 @@
+// import express from 'express';
+// import Hello from './Hello.js';
+// import Lab5 from './Lab5.js';
+// import cors from "cors";
+// import CourseRoutes from './Kanbas/courses/routes.js';
+// import ModuleRoutes from "./Kanbas/modules/routes.js";
+// import AssignmentRoutes from './Kanbas/assignments/routes.js';
+// import UserRoutes from "./Kanbas/users/routes.js";
+// import mongoose from "mongoose";
+// import session from "express-session";
+// import "dotenv/config";
+
+// const app = express();
+
+// mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: process.env.FRONTEND_URL
+//   })
+//  );
+ 
+// app.use(express.json());
+
+// const sessionOptions = {
+//   secret: process.env.SESSION_SECRET,
+//   resave: false,
+//   saveUninitialized: false,
+// };
+
+// if (process.env.NODE_ENV !== "development") {
+//   sessionOptions.proxy = true;
+//   sessionOptions.cookie = {
+//     sameSite: "none",
+//     secure: true,
+//     domain: process.env.HTTP_SERVER_DOMAIN,
+//   };
+// }
+
+// console.log(sessionOptions);
+
+
+// app.use(
+//   session(sessionOptions)
+// );
+  
+
+// Hello(app);
+// Lab5(app);
+// CourseRoutes(app);
+// ModuleRoutes(app);
+// AssignmentRoutes(app);
+// UserRoutes(app);
+
+// app.listen(process.env.PORT || 4000);
+
 import express from 'express';
 import Hello from './Hello.js';
 import Lab5 from './Lab5.js';
@@ -5,15 +62,40 @@ import cors from "cors";
 import CourseRoutes from './Kanbas/courses/routes.js';
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentRoutes from './Kanbas/assignments/routes.js';
+import UserRoutes from "./Kanbas/users/routes.js";
+import mongoose from "mongoose";
+import session from "express-session";
+import "dotenv/config";
 
 const app = express();
 app.use(cors());
+
 app.use(express.json());
+
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.HTTP_SERVER_DOMAIN,
+  };
+}
+app.use(session(sessionOptions));
+
+const CONNECTION_STRING = "mongodb://127.0.0.1:27017/kanbas"
+  // process.env.DB_CONNECTION_STRING || process.env.MONGODB;
+mongoose.connect(CONNECTION_STRING);
 
 Hello(app);
 Lab5(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
+UserRoutes(app);
 
 app.listen(process.env.PORT || 4000);
